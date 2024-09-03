65d45f52ee0ae999c97fbdc80ff812015d836ce2
Cooper Weinhold <cjweinhold@mail.fhsu.edu>
2024-09-03 12:33:59 -0500

    Add files via upload
    Adding assignment 2.

---
+++ main.js
@@ -0,0 +1,48 @@
+//Challenge 1
+let age, student, nonStudent;
+const name = "Cooper";
+age = 21; 
+student = 1;
+nonStudent = 0;
+console.log(name);
+console.log(age);
+console.log(student>nonStudent);
+
+//Challenge 2
+let x,y;
+x = 20, y= 5;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+//Challenge 3
+const string1 = "This is my sentence for challenge 3.";
+console.log(string1.length);
+console.log(string1.charAt(0));
+console.log(string1.charAt(35));
+
+//Challenge 4
+let z;
+z = -49;
+console.log(Math.sqrt(z));
+console.log(Math.pow(z, 2));
+console.log(Math.abs(z));
+
+//Challenge 5
+let a,b;
+a = 1, b = 10;
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+
+//Challenge 6
+let pig = true, cow = false; 
+console.log(pig && cow);
+console.log(pig || cow);
+console.log(pig =! cow);
+
+//Challenge 7
+let lastName = "Bob", firstName = "Freak"; 
+let greeting = `Hello, ${firstName} ${lastName}`;
+console.log(greeting);
